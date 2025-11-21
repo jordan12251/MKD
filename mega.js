7b33c2a49aa2cd76334a27705e5fdddb311c03d1
jordan12251 <dosantosjotham@gmail.com>
2025-11-21 21:36:58 +0200

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,27 +1,45 @@
-const mega = require('megajs');
+// mega.js
+import mega from "megajs";
 
 // Infos du compte Mega
-let email = 'Dosantosjotham@gmail.com';
-let password = 'Le28022008!';
+const email = 'Dosantosjotham@gmail.com';
+const password = 'Le28022008!';
 
 // Connexion au compte Mega
 const storage = mega({ email, password });
 
-// Fonction pour uploader un fichier
-function uploadFile(filePath) {
-    storage.on('ready', () => {
-        const file = storage.upload({ name: filePath.split('/').pop() }, filePath);
-        file.on('complete', () => {
-            file.link((err, url) => {
-                if (err) {
-                    console.error('Erreur génération du lien:', err);
-                } else {
-                    console.log('Lien du fichier:', url);
-                }
+/**
+ * Fonction pour uploader un fichier sur Mega et récupérer le lien
+ * @param {ReadableStream|Buffer|String} fileStream - Le fichier ou chemin du fichier à uploader
+ * @param {String} fileName - Nom du fichier sur Mega
+ * @returns {Promise<String>} - Retourne l'URL du fichier
+ */
+export async function upload(fileStream, fileName) {
+    return new Promise((resolve, reject) => {
+        storage.on('ready', () => {
+            const file = storage.upload({ name: fileName }, fileStream);
+
+            file.on('complete', () => {
+                file.link((err, url) => {
+                    if (err) {
+                        console.error('Erreur génération du lien:', err);
+                        reject(err);
+                    } else {
+                        console.log('Lien du fichier:', url);
+                        resolve(url);
+                    }
+                });
             });
+
+            file.on('error', (err) => {
+                console.error('Erreur upload Mega:', err);
+                reject(err);
+            });
+        });
+
+        storage.on('error', (err) => {
+            console.error('Erreur connexion Mega:', err);
+            reject(err);
         });
     });
 }
-
-// Exemple : uploader un fichier nommé 'document.txt'
-uploadFile('document.txt');
