6405b7f01f03951495de528dc521623a6fd91268
jordan12251 <dosantosjotham@gmail.com>
2025-11-21 19:42:44 +0200

    Update gen-id.js

--- gen-id.js
+++ gen-id.js
@@ -1,10 +1,9 @@
-function makeid(num = 4) {
+export function makeid(num = 4) {
   let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var characters9 = characters.length;
-  for (var i = 0; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters9));
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < num; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 }
-module.exports = {makeid};
