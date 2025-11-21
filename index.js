fcafe03eaabd4925d001fa11b5b1b1d8573155d2
jordan12251 <dosantosjotham@gmail.com>
2025-11-21 19:40:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
-const express = require('express');
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import server from "./qr.js";
+import code from "./pair.js";
+
+process.setMaxListeners(500);
+
 const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/server', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/qr',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
+// __dirname replacement in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Routes
+app.use("/server", server);
+app.use("/code", code);
+
+app.use("/pair", (req, res) => {
+    res.sendFile(path.join(__dirname, "pair.html"));
+});
+
+app.use("/qr", (req, res) => {
+    res.sendFile(path.join(__dirname, "qr.html"));
+});
+
+app.use("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "main.html"));
+});
+
+// Body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Server
 app.listen(PORT, () => {
     console.log(`
-Don't Forgot To Give Star MALVIN-XD
+Don't Forget To Give A Star MALVIN-XD ⭐
 
- Server running on http://localhost:` + PORT)
-})
+Server running on http://localhost:${PORT}
+`);
+});
 
-module.exports = app
+export default app;
