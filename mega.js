a07adb17720ba2fe749ec1dafb8be220f7915216
jordan12251 <dosantosjotham@gmail.com>
2025-11-22 00:27:29 +0200

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,32 +1,32 @@
-// mega.js
 import mega from "megajs";
 import fs from "fs";
 
-// Infos du compte Mega
-const email = 'Dosantosjotham@gmail.com';
-const password = 'Le28022008!';
-
-export async function uploadFile(filePath, fileName) {
-    return new Promise((resolve, reject) => {
-        const storage = mega({ email, password });
-        
-        storage.on('ready', () => {
-            const file = storage.upload({ name: fileName }, filePath);
-            
-            file.on('complete', () => {
-                file.link((err, url) => {
-                    if (err) return reject(err);
-                    resolve(url);
-                });
-            });
-            
-            file.on('error', (err) => {
-                reject(err);
-            });
-        });
-
-        storage.on('error', (err) => {
-            reject(err);
-        });
+/**
+ * Upload sur Mega
+ * @param {fs.ReadStream} fileStream 
+ * @param {string} name 
+ * @returns {Promise<string>} Lien du fichier
+ */
+export function uploadFile(fileStream, name) {
+  return new Promise((resolve, reject) => {
+    const storage = mega({
+      email: "Dosantosjotham@gmail.com",
+      password: "Le28022008!"
     });
+
+    storage.on("ready", () => {
+      const file = storage.upload({ name }, fileStream);
+
+      file.on("complete", () => {
+        file.link((err, url) => {
+          if (err) return reject(err);
+          resolve(url);
+        });
+      });
+
+      file.on("error", (err) => reject(err));
+    });
+
+    storage.on("error", (err) => reject(err));
+  });
 }
