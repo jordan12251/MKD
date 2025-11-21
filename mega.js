9f140257e28d3c87e3d955d01b51ff5f99cd9134
jordan12251 <dosantosjotham@gmail.com>
2025-11-21 23:00:59 +0200

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,23 +1,44 @@
+// mega.js
 import mega from "megajs";
 
+// Infos du compte MEGA
+const email = 'Dosantosjotham@gmail.com';
+const password = 'Le28022008!';
+
+/**
+ * Upload un fichier sur MEGA
+ * @param {Stream} fileStream - Stream du fichier à uploader
+ * @param {string} fileName - Nom du fichier à créer sur MEGA
+ * @returns {Promise<string>} - Lien du fichier MEGA
+ */
 export async function upload(fileStream, fileName) {
     return new Promise((resolve, reject) => {
-        const storage = mega({
-            email: "Dosantosjotham@gmail.com",
-            password: "Le28022008!"
-        });
+        const storage = mega({ email, password });
 
-        storage.on("ready", () => {
+        storage.on('ready', () => {
             const file = storage.upload({ name: fileName }, fileStream);
 
-            file.on("complete", () => {
+            file.on('complete', () => {
                 file.link((err, url) => {
-                    if (err) reject(err);
-                    else resolve(url);
+                    if (err) {
+                        console.error('Erreur génération du lien:', err);
+                        reject(err);
+                    } else {
+                        console.log('Lien du fichier:', url);
+                        resolve(url);
+                    }
                 });
+            });
+
+            file.on('error', (err) => {
+                console.error('Erreur lors de l\'upload:', err);
+                reject(err);
             });
         });
 
-        storage.on("error", (err) => reject(err));
+        storage.on('error', (err) => {
+            console.error('Erreur connexion MEGA:', err);
+            reject(err);
+        });
     });
 }
