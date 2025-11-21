f92dd4542f37b623914f9d42d935f12907830149
jordan12251 <dosantosjotham@gmail.com>
2025-11-21 22:40:15 +0200

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,44 +1,23 @@
-const mega = require("megajs");
-const fs = require("fs");
-const path = require("path");
+import mega from "megajs";
 
-// Identifiants MEGA
-const email = "Dosantosjotham@gmail.com";
-const password = "Le28022008!";
-
-// Fonction pour uploader un fichier vers MEGA
-async function uploadFileToMega(filePath) {
+export async function upload(fileStream, fileName) {
     return new Promise((resolve, reject) => {
-        const storage = mega({ email, password });
+        const storage = mega({
+            email: "Dosantosjotham@gmail.com",
+            password: "Le28022008!"
+        });
 
         storage.on("ready", () => {
-            const fileName = path.basename(filePath);
-
-            const file = storage.upload(
-                { name: fileName },
-                fs.createReadStream(filePath)
-            );
+            const file = storage.upload({ name: fileName }, fileStream);
 
             file.on("complete", () => {
                 file.link((err, url) => {
-                    if (err) {
-                        console.error("Erreur MEGA:", err);
-                        reject(err);
-                    } else {
-                        console.log("Lien MEGA:", url);
-                        resolve(url);
-                    }
+                    if (err) reject(err);
+                    else resolve(url);
                 });
             });
         });
 
-        storage.on("error", (err) => {
-            console.error("Erreur connexion MEGA:", err);
-            reject(err);
-        });
+        storage.on("error", (err) => reject(err));
     });
 }
-
-module.exports = {
-    uploadFileToMega
-};
